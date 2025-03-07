baee748b7b15725b364f91647fdbb0afe977b7ba
Rajiv Roy <rajivpune18@gmail.com>
2025-03-07 13:42:32 +0530

    separate counter logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,10 @@
-import { useState } from "react";
 import "./App.css";
 import Counter from "./Counter";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const handleClick = (action) => {
-    if (action === "increase") {
-      setCount(count + 1);
-    }
-    if (action === "decrease") {
-      setCount(count - 1);
-    }
-    if (action === "reset") {
-      setCount(0);
-    }
-  };
-
   return (
     <>
       <Counter />
-      <div>
-        <label>{count}</label>
-      </div>
-      <div>
-        <button className="btn-action" onClick={() => handleClick("increase")}>
-          Increment
-        </button>
-        <button className="btn-action" onClick={() => handleClick("decrease")}>
-          Decrement
-        </button>
-        <button className="btn-action" onClick={() => handleClick("reset")}>
-          Reset
-        </button>
-      </div>
     </>
   );
 }
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { useState } from "react";
 
 const Counter = () => {
-  return <div>Counter</div>;
+  const [count, setCount] = useState(0);
+
+  const handleClick = (action) => {
+    if (action === "increase") {
+      setCount(count + 1);
+    }
+    if (action === "decrease") {
+      setCount(count - 1);
+    }
+    if (action === "reset") {
+      setCount(0);
+    }
+  };
+
+  return (
+    <>
+      <div>Counter</div>
+      <div>
+        <label>{count}</label>
+      </div>
+      <div>
+        <button className="btn-action" onClick={() => handleClick("increase")}>
+          Increment
+        </button>
+        <button className="btn-action" onClick={() => handleClick("decrease")}>
+          Decrement
+        </button>
+        <button className="btn-action" onClick={() => handleClick("reset")}>
+          Reset
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Counter;
