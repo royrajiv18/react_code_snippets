577c6cfaab007d82bccfbb124315473b12bba0c5
Rajiv Roy <rajivpune18@gmail.com>
2025-03-29 06:51:01 -0700

    Create Autofocus.jsx

---
+++ src/Autofocus.jsx
@@ -0,0 +1,12 @@
+/*
+ autofocus an input on mount
+*/
+function App() {
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  return <input ref={inputRef} type="text" />;
+}
