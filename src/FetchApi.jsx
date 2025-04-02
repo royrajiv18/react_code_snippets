a63e0bd66b8a9a706a304c37e8619ceed038713e
Rajiv Roy <rajivpune18@gmail.com>
2025-04-01 19:21:30 -0700

    making an arrow function

--- src/FetchApi.jsx
+++ src/FetchApi.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-function UsersList() {
+const UsersList = ()=> {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
 
